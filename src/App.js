5d836e2f14eb895938f3370029943394710a70bf
Taimoor M <taimoor.m89@gmail.com>
2020-12-11 15:05:16 -0500

    first commit - setup navbar

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Check Out</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          {/* This is the default route  */}
+          <Route path="/">
+            <Header />
+            <h1>Amazon HomePage</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCH3Bf8dPO4op2vhMeSQ1qD6Ch45du8gZI",
+    authDomain: "clone-45ccc.firebaseapp.com",
+    databaseURL: "https://clone-45ccc.firebaseio.com",
+    projectId: "clone-45ccc",
+    storageBucket: "clone-45ccc.appspot.com",
+    messagingSenderId: "1010339240113",
+    appId: "1:1010339240113:web:5cee07c7bddb0f31372a90",
+    measurementId: "G-YTEQEV6MBC"
+};
