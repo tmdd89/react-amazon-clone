8e734e6473659bce069cddc6254717c8e5e14529
Taimoor M <taimoor.m89@gmail.com>
2020-12-11 19:53:10 -0500

    setup contextAPI

---
+++ src/StateProvider.js
@@ -0,0 +1,13 @@
+// setup data layer 
+//for tracking basket 
+import React, { createContext, useContext, useReducer } from 'react'
+
+//DATA LAYER
+export const StateContext = createContext();
+
+//BUILD PROVIDER
+export const StateProvider = ({ reducer, initialState,children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+)
